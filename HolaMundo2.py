bfff91f18b5f79e8422bcf9e65dd3c0e91467ed3
Andres Narvaez <andresnarvaez99@hotmail.com>
2021-08-17 21:57:07 -0500

    Primer Cambio

--- HolaMundo2.py
+++ HolaMundo2.py
@@ -2,3 +2,9 @@ print ("Hola Mundo")
 numero1 = int(input("Introduce un número: "))
 numero2 = int(input("Introduce otro número: "))
 print ("La suma de ", numero1, "más ", numero2, " es ", numero1 + numero2)
+
+print ("Strings - Cadenas de texto")
+cadena1 = "Esto es una prueba de cadenas de texto en Python por ProyectoA"
+cadena2 = "Concatenaremos y extraeremos texto de estas cadenas"
+print ("Concatenamos cadena1 y cadena2")
+print (cadena1,cadena2)
