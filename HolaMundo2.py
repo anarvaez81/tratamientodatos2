4937bf98573c67fa089e9d9ff6ec4ee2e957b11a
Andres Narvaez <andresnarvaez99@hotmail.com>
2021-08-17 21:49:13 -0500

    Codigo Inicial

---
+++ HolaMundo2.py
@@ -0,0 +1,4 @@
+print ("Hola Mundo")
+numero1 = int(input("Introduce un número: "))
+numero2 = int(input("Introduce otro número: "))
+print ("La suma de ", numero1, "más ", numero2, " es ", numero1 + numero2)
